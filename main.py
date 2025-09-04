8dab07d0a498e8cff0116e6d90cbf5b6e22da551
Harish5nu <saudharish770@gmail.com>
2025-09-04 22:48:18 +0545

    with readme

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#
 '''
 1 for snake
 -1 for water
@@ -12,29 +13,54 @@ reverseDict={1 :"snake",-1 : "water",0: "Gun"}
 you=youDict[youstr]    
 print(f"You choose {reverseDict[you]} and Computer choose {reverseDict[computer]}")
 
-# if(computer==-1 and you==1):
-#     print("You win.")
-# elif(computer==-1 and you==0):
-#     print("You lose.")
-
-
-
-# elif(computer==1 and you==-1):
-#     print("You lose")
-# elif(computer==1 and you==0):
-#     print("You win")
-
-
-# elif(computer==0 and you==-1):
-#     print("You win")
-# elif(computer==0 and you==1):
-#     print("You lose")
-
-# elif(computer==you):
-#     print("Game draw")
-if(computer==you):
-    print("Game draw")
-elif((computer-you)==-1 or  (computer-you)==2):
+if(computer==-1 and you==1): 
+    print("You win.")
+elif(computer==-1 and you==0):
     print("You lose.")
-else:
+
+
+
+elif(computer==1 and you==-1):
+    print("You lose")
+elif(computer==1 and you==0):
     print("You win")
+
+
+elif(computer==0 and you==-1):
+    print("You win")
+elif(computer==0 and you==1):
+    print("You lose")
+
+elif(computer==you):
+    print("Game draw")
+
+#Alternative Method
+'''
+if(computer==-1 and you==1): # (computer-you)=-2
+    print("You win.")
+elif(computer==-1 and you==0): # (computer-you)=-1
+    print("You lose.")
+
+
+
+elif(computer==1 and you==-1): # (computer-you)=2
+    print("You lose")
+elif(computer==1 and you==0): # (computer-you)=1
+    print("You win")
+
+
+elif(computer==0 and you==-1): # (computer-you)=1
+    print("You win")
+elif(computer==0 and you==1): # (computer-you)=-1
+    print("You lose")
+'''
+
+'''
+
+ if(computer==you):
+     print("Game draw")
+ elif((computer-you)==-1 or  (computer-you)==2):
+     print("You lose.")
+ else:
+     print("You win")
+'''
